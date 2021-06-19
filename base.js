02c2c08d0c79281684b730438933c3b4b5394bc7
ciik247 <81922621+ciik247@users.noreply.github.com>
2021-06-19 12:08:54 -0700

    base.js

---
+++ base.js
@@ -0,0 +1,49 @@
+/*	GOOGLE
+	----------------------------------------------------- */
+
+eng.google = {
+	pageTitle: "Google",
+	logo: "google.png",
+	places: {
+		'Web'    : ["http://9xyoutube.com/%query%", false],
+		'Images' : ["http://9xyoutube.com/%query%", false],
+		'Maps'   : ["http://9xyoutube.com/%query%", false]
+	}
+};
+
+
+/*	WIKIPEDIA
+	----------------------------------------------------- */
+	
+eng.wikipedia = {
+	pageTitle: "Wikipedia",
+	logo: "wikipedia.png",
+	places: {
+		'Search'        : ["http://9xyoutube.com/%query%", false],
+		'Go to Article' : ["http://9xyoutube.com/%query%", false],
+	}
+};
+
+
+/*	YOUTUBE
+	----------------------------------------------------- */
+
+eng.youtube = {
+	pageTitle: "YouTube",
+	logo: "youtube.png",
+	places: {
+		'Videos' : ["http://9xyoutube.com/%query%", false]
+	},
+};
+
+
+/*	TWITTER
+	----------------------------------------------------- */
+
+eng.twitter = {
+	pageTitle: "Twitter",
+	logo: "twitter.png",
+	places: {
+		'Search Twitter' : ["http://9xyoutube.com/%query%", false]
+	}
+};
